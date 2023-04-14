4a29411f0a508ea76d2d746c5d81ad08989afe01
Bruno <bruno.rodriguez@student-cs.fr>
2023-04-14 17:32:27 -0400

    Repository Initialization

---
+++ src/scenes/agregar_vehiculo/index.jsx
@@ -0,0 +1,171 @@
+import { Box, Button, TextField } from "@mui/material";
+import { Formik } from "formik";
+import * as yup from "yup";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+
+const AgregarVehiculo = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="AREGAR VEHÍCULO" subtitle="Ingresa la infomación del nuevo vehÍculo" />
+
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={checkoutSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box
+              display="grid"
+              gap="30px"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+              sx={{
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+              }}
+            >
+              <TextField
+                // fullWidth
+                variant="filled"
+                type="text"
+                label="Patente"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.patente}
+                name="patente"
+                error={!!touched.patente && !!errors.patente}
+                helperText={touched.patente && errors.patente}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                // fullWidth
+                variant="filled"
+                type="text"
+                label="Kilometraje"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.kilometraje}
+                name="kilometraje"
+                error={!!touched.kilometraje && !!errors.kilometraje}
+                helperText={touched.kilometraje && errors.kilometraje}
+                sx={{ gridColumn: "span 2" }}
+                inputProps={{
+                  inputMode: 'numeric',
+                  pattern: '[0-9]*',
+                  inputProps: { min: 0 }
+                }}
+              />
+              <TextField
+                // fullWidth
+                variant="filled"
+                type="text"
+                label="Tipo"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.tipo}
+                name="tipo"
+                error={!!touched.tipo && !!errors.tipo}
+                helperText={touched.tipo && errors.tipo}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                // fullWidth
+                variant="filled"
+                type="text"
+                label="Clase"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.clase}
+                name="clase"
+                error={!!touched.clase && !!errors.clase}
+                helperText={touched.clase && errors.clase}
+                sx={{ gridColumn: "span 2" }}
+              />
+              <TextField
+                // fullWidth
+                variant="filled"
+                type="text"
+                label="Conductor"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.conductor}
+                name="conductor"
+                error={!!touched.conductor && !!errors.conductor}
+                helperText={touched.conductor && errors.conductor}
+                sx={{ gridColumn: "span 2" }}
+              />
+              
+              <TextField
+                // fullWidth
+                variant="filled"
+                type="text"
+                label="Contrato"
+                onBlur={handleBlur}
+                onChange={handleChange}
+                value={values.contrato}
+                name="contrato"
+                error={!!touched.contrato && !!errors.contrato}
+                helperText={touched.contrato && errors.contrato}
+                sx={{ gridColumn: "span 2" }}
+              />
+            </Box>
+            <Box mt="20px"
+             sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Button
+                variant="contained"
+                component="label"
+              >
+                Subir Foto
+                <input
+                  type="file"
+                  hidden
+                />
+              </Button>
+              <Button type="submit" color="secondary" variant="contained">
+                Agregar Vehiculo
+              </Button>
+            </Box>
+          </form>
+        )}
+      </Formik>
+    </Box>
+  );
+};
+
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup
+    .string()
+    .matches(phoneRegExp, "Phone number is not valid")
+    .required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
+export default AgregarVehiculo;
---
+++ src/scenes/ficha_vehiculo/index.jsx
@@ -0,0 +1,131 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+function createData(patente, tipo, clase, disponibilidad, kilometraje, conductor) {
+  return {patente, tipo, clase, disponibilidad, kilometraje, conductor};
+}
+
+const rows = [
+  createData("Patente:", "ABC123", "Disponibilidad:", "Disponible"),
+  createData("Tipo de vehículo:", "Sedán",  "Kilometraje:", 2000),
+  createData("Clase de vehículo:", "Clase A", "Conductor:", "Juan Pérez",)
+];
+
+const FichaVehiculo = () => {
+  const isNonMobile = useMediaQuery("(min-width:600px)");
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box m="20px">
+      <Header title="FICHA VEHÍCULO" subtitle="Toda la información del vehículo en un solo lugar" />
+      {/* patente, tipo, clase, disponibilidad, kilometraje, conductor */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="caption table">
+          <caption>Información estática y dinámica del vehículo</caption>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell align="left"> <Typography color={colors.grey[500]} variant="h5">{row.patente}</Typography></TableCell>
+                <TableCell align="left">{row.tipo}</TableCell>
+                <TableCell align="left"> <Typography color={colors.grey[500]} variant="h5">{row.clase}</Typography></TableCell>
+                <TableCell align="left">{row.disponibilidad}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Box sx={{mt:8}}>
+
+      <Accordion >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.grey[500]} variant="h4">
+            TAREAS
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.grey[500]} variant="h4">
+            FISCALIZACIONES
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.grey[500]} variant="h4">
+            CONDUCTORES
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.grey[500]} variant="h4">
+            REVISIÓNES TÉCNICAS
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.grey[500]} variant="h4">
+            MANTENCIONES
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+    </Box>
+  );
+};
+
+
+
+export default FichaVehiculo;
